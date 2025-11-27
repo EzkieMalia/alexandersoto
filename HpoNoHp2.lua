ae736009e66bbd94147b6cecc785071bfec0538b
EzkieMalia <ezekielquisky1z@outlook.com>
2025-11-26 18:38:38 -0700

    Update HpoNoHp2.lua

--- HpoNoHp2.lua
+++ HpoNoHp2.lua
@@ -51,29 +51,20 @@ for i = 0, 2000, 1 do
   print("Request... ACCEPTED.")
 end
 
-task.wait(2)
-print("Loading Main Gui")
-
-while Dark2 == true do
 task.wait(30)
 local Frame = Instance.new("Frame")
 Frame.Parent = ScreenGui
 Frame.Size = UDim2.new(0.5, 0, 0.5, 0)
 Frame.Position = UDim2.new(.25, 0, .25, 0)
 Frame.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-end
 
-while Dark2 == true do
 local FrameStroke = Instance.new("UIStroke")
 FrameStroke.Parent = Frame
 FrameStroke.Thickness = 2
-end
 
-while Dark2 == true do
 local FrameCorner = Instance.new("UICorner")
 FrameCorner.Parent = Frame
 FrameCorner.CornerRadius = UDim.new(.01)
-end
 
 task.wait(2)
 
